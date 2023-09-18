bb048805fd9a7749f08928be35922538cecb12a7
Kristina <Bolshakovakristy@gmail.com>
2023-09-18 19:45:27 +0300

    Initial commit

---
+++ src/main/java/org/example/L1/Calculator.java
@@ -0,0 +1,37 @@
+package org.example.L1;
+
+public class Calculator {
+
+//    public static void main(String[] args) {
+//        System.out.printf("Результат операции: %s\n", calculation(2, 2, '+'));
+//        System.out.printf("Результат операции: %s\n", calculation(4, 2, '-'));
+//        System.out.printf("Результат операции: %s\n", calculation(8, 2, '/'));
+//        System.out.printf("Результат операции: %s\n", calculation(3, 2, '*'));
+//    }
+
+    public static int calculation(int firstOperand, int secondOperand, char operator) {
+        int result = 0;
+
+        switch (operator) {
+            case '+':
+                result = firstOperand + secondOperand;
+                break;
+            case '-' :
+                result = firstOperand - secondOperand;
+                break;
+            case '*':
+                result = firstOperand * secondOperand;
+                break;
+            case '/':
+                if (secondOperand != 0){
+                    result = firstOperand / secondOperand;
+                    break;
+                } else {
+                    throw new ArithmeticException("Division by zero is not possible");
+                }
+            default :
+                throw new IllegalArgumentException("Unexpected value operator: " + operator);
+        }
+        return result;
+    }
+}
---
+++ src/main/java/org/example/S1/tasks/Task3.java
@@ -0,0 +1,24 @@
+package org.example.S1.tasks;
+
+/**
+ * **Задание:** Нужно исправить метод сложения двух чисел так, чтобы при переполнении переменной (Запустить пример  System.out.println(sum(2_147_483_647, 2)) ) выбрасывалось предупреждение
+ * <p>
+ * ```java
+ * public static int sum(int a, int b) {
+ * <p>
+ * return a+b;
+ * }
+ * ```
+ */
+public class Task3 {
+    public static void main(String[] args) {
+        System.out.println(sum(2_147_483_647, 2));
+    }
+
+    public static int sum(int a, int b) {
+        String msg = "Слишком большое число";
+        int sum = a + b;
+        assert sum > Integer.MAX_VALUE : msg;
+        return sum;
+    }
+}
